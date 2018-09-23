b72aba02990fd3b9f30b1f27f8bee1e270eb2b9c
Administrator <Administrator@USER-20161204LV>
2018-09-23 16:27:44 +0800

    remove redundant code

--- maven_project/src/main/java/com/maven/DemoApplication.java
+++ maven_project/src/main/java/com/maven/DemoApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-//@MapperScan("com.maven.mapper")
 @EnableCaching
 public class DemoApplication {
 
