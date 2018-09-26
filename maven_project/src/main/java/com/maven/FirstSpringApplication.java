a0f51e4a227e1c7da76047c62a6c654b185f0f70
skymanRedom <447984228@qq.com>
2018-09-26 21:58:23 +0800

    for test

--- maven_project/src/main/java/com/maven/FirstSpringApplication.java
+++ maven_project/src/main/java/com/maven/FirstSpringApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cache.annotation.EnableCaching;
 @EnableCaching
 public class FirstSpringApplication {
 
+	// test git
 	public static void main(String[] args) {
 		SpringApplication.run(FirstSpringApplication.class, args);
 	}
