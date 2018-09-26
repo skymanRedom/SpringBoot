04baa6ac2b70553201c6eb1b07025c870bd226e3
skymanRedom <447984228@qq.com>
2018-09-26 21:52:20 +0800

    rename main class to FirstSpring

--- maven_project/src/main/java/com/maven/FirstSpringApplication.java
+++ maven_project/src/main/java/com/maven/FirstSpringApplication.java
@@ -6,9 +6,9 @@ import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
-public class DemoApplication {
+public class FirstSpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(FirstSpringApplication.class, args);
 	}
 }
